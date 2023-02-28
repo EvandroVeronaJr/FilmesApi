a6198c632de73a2fddce8f207de8a258c295b8e2
DESKTOP-ME7S2FS\Evandro Jr <verona.junior@gmail.com>
2023-02-28 17:00:29 -0300

    Initial commit

---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesApi.Controllers.Models;
+
+public class Filme
+{
+    public String Titulo { get; set; }
+    public String Genero { get; set; }
+    public float Duracao { get; set; }
+
+}
